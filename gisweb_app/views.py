01768b32d90f9205bff6d74571bbcc168be09c85
gismanli <gismanli@163.com>
2015-05-21 19:30:14 +0800

    .

--- gisweb_app/views.py
+++ gisweb_app/views.py
@@ -25,7 +25,7 @@ def data_resource(request, uploaded=''):
     UPLODED_FILES = []
     for each in ALL_FILES:
         file_format = what_format(MEDIA_ROOT+MEDIA_URL+each)
-        print file_format
+        #print file_format
         if each.split(".")[-1]=="shp":
             UPLODED_FILES.append(each)
         if each.split(".")[-1]=="shx":
